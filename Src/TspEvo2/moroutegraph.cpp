3bf6e17534ce0d84acffc7e4d1f45520fc2988ad
dominicusplatus <dominik.guz11@gmail.com>
2016-12-10 19:18:44 +0100

    routegraph restore

--- Src/TspEvo2/moroutegraph.cpp
+++ Src/TspEvo2/moroutegraph.cpp
@@ -4,12 +4,17 @@
 #include <iostream>
 #include <math.h>
 
+#include "moroutegraph.h"
+
+#define gabs(x) (x<0)?-x:x
+
 namespace MORouteGraph {
 
   static std :: vector <std :: pair <double, double> > vectCoord ; // Coordinates
 
-  static std :: vector <std :: vector <unsigned> > dist ; // Distances Mat.
-  static std :: vector <double>  costs; // Costs Mat.
+  static std :: vector <std :: vector <double> > dist ; // Distances Mat.
+  static std :: vector <std :: vector <double> > costs ; // Costs Mat.
+
 
   unsigned size () {
 
@@ -17,6 +22,23 @@ namespace MORouteGraph {
   }
 
   void computeDistances () {
+      // Dim.
+         unsigned numCities = vectCoord.size () ;
+         dist.resize (numCities) ;
+         for (unsigned i = 0 ; i < dist.size () ; i ++)
+           dist [i].resize (numCities) ;
+
+         // Computations.
+         for (unsigned i = 0 ; i < dist.size () ; i ++)
+           for (unsigned j = i + 1 ; j < dist.size () ; j ++) {
+             double distX = vectCoord [i].first - vectCoord [j].first ;
+             double distY = vectCoord [i].second - vectCoord [j].second ;
+             dist [i] [j] = dist [j] [i] = (unsigned) (sqrt ((float) (distX * distX + distY * distY)) + 0.5) ;
+     }
+  }
+
+
+  void computeDistances2 () {
 
     // Dim.
     unsigned numCities = vectCoord.size () ;
@@ -27,26 +49,48 @@ namespace MORouteGraph {
     // Computations.
     for (unsigned i = 0 ; i < dist.size () ; i ++)
       for (unsigned j = i + 1 ; j < dist.size () ; j ++) {
-        double distX = vectCoord [i].first - vectCoord [j].first ;
-        double distY = vectCoord [i].second - vectCoord [j].second ;
-        dist [i] [j] = dist [j] [i] = (unsigned) (sqrt ((float) (distX * distX + distY * distY)) + 0.5) ;
+        double distX =  abs( vectCoord [i].first - vectCoord [j].first) ;
+        double distY =  abs(vectCoord [i].second - vectCoord [j].second) ;
+        double len =  (sqrt (distX * distX + distY * distY));
+        dist [i] [j] = dist [j] [i] =  len;
       }
   }
 
+
   void computeCosts()
   {
-        //TODO
       unsigned numCities = vectCoord.size () ;
-      dist.resize (numCities) ;
-      for (unsigned i = 0 ; i < dist.size () ; i ++)
-        dist [i].resize (numCities) ;
+      costs.resize (numCities) ;
+      for (unsigned i = 0 ; i < costs.size () ; i ++)
+        costs [i].resize (numCities) ;
 
-      // Computations.
-      for (unsigned i = 0 ; i < dist.size () ; i ++)
-        for (unsigned j = i + 1 ; j < dist.size () ; j ++) {
-          double distX = vectCoord [i].first - vectCoord [j].first ;
-          double distY = vectCoord [i].second - vectCoord [j].second ;
-          dist [i] [j] = dist [j] [i] = (unsigned) (sqrt ((float) (distX * distX + distY * distY)) + 0.5) ;
+      for (unsigned i = 0 ; i < costs.size () ; i ++)
+        for (unsigned j = i + 1 ; j < costs.size () ; j ++) {
+            double distX =  ( vectCoord [i].first - vectCoord [j].first) ;
+            double distY =  (vectCoord [i].second - vectCoord [j].second) ;
+          int vertexDiff = gabs(j-i);
+          unsigned len =  (unsigned) (sqrt ((float) (distX * distX + distY * distY)) + 0.5) ;
+          unsigned cost =(unsigned) (len * 1/numCities * vertexDiff) ;
+          costs [i] [j] = dist [j] [i] = cost;
+        }
+  }
+
+  void computeCosts2()
+  {
+      unsigned numCities = vectCoord.size () ;
+      costs.resize (numCities) ;
+      for (unsigned i = 0 ; i < costs.size () ; i ++)
+        costs [i].resize (numCities) ;
+
+      for (unsigned i = 0 ; i < costs.size () ; i ++)
+        for (unsigned j = i + 1 ; j < costs.size () ; j ++) {
+            double distX = abs ( vectCoord [i].first - vectCoord [j].first) ;
+            double distY = abs (vectCoord [i].second - vectCoord [j].second) ;
+          int vertexDiff = gabs(j-i);
+           double len =  (sqrt (distX * distX + distY * distY));
+          double cost = len * 1/numCities * vertexDiff;
+          unsigned int costi =  len * (1/(numCities)*(j-i)) +1 ;
+          costs [i] [j] = dist [j] [i] = costi;
         }
   }
 
@@ -67,13 +111,13 @@ namespace MORouteGraph {
       for (unsigned i = 0 ; i < num_vert ; i ++)
     {
         std::pair <double, double>  vectCoordTmp;
-        f >> vectCoord [i].first >> vectCoord [i].second >> vectCoordTmp.first;
-        costs[i] = vectCoordTmp.first;
+        f >> vectCoord [i].first >> vectCoord [i].second;   //>> vectCoordTmp.first;
+        //vertcost[i] = vectCoordTmp.first;
     }
       f.close () ;
 
-      computeDistances () ;
-      computeCosts();
+      computeDistances2 () ;
+      computeCosts2();
 
     }
     else {
@@ -83,14 +127,14 @@ namespace MORouteGraph {
     }
   }
 
-  float distance (unsigned __from, unsigned __to) {
+  double distance (unsigned __from, unsigned __to) {
 
     return dist [__from] [__to] ;
   }
 
- float cost(unsigned __from, unsigned __to) {
+ double cost(unsigned __from, unsigned __to) {
 
-    return dist [__from] [__to] ;
+    return costs [__from] [__to] ;
   }
 
  std::pair <double, double> getCityCoords(unsigned city)
